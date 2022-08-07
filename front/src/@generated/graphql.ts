c783fd8ed99777dc01de503a3631e8bdf13d405d
takeru.hashimoto <hstake25@gmail.com>
2022-08-07 16:32:13 +0900

    graphql-codegen generate

---
+++ front/src/@generated/graphql.ts
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  _empty?: Maybe<Scalars['String']>;
+  createTodo: Todo;
+  createUser: User;
+};
+
+
+export type MutationCreateTodoArgs = {
+  input: NewTodo;
+};
+
+
+export type MutationCreateUserArgs = {
+  input: NewUser;
+};
+
+export type NewTodo = {
+  text: Scalars['String'];
+  userId: Scalars['String'];
+};
+
+export type NewUser = {
+  name: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  _empty?: Maybe<Scalars['String']>;
+  todos: Array<Todo>;
+  users: Array<User>;
+};
+
+export type Todo = {
+  __typename?: 'Todo';
+  done: Scalars['Boolean'];
+  id: Scalars['ID'];
+  text: Scalars['String'];
+  user: User;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
